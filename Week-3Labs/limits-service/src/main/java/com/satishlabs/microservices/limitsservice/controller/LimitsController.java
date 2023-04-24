0bd42476e92bf68d1ce76ca511cca22d27879cac
satishlabs <spd61185@gmail.com>
2023-04-24 18:38:23 +0530

    Spring config server

---
+++ Week-3Labs/limits-service/src/main/java/com/satishlabs/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,20 @@
+package com.satishlabs.microservices.limitsservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.satishlabs.microservices.limitsservice.bean.Limits;
+import com.satishlabs.microservices.limitsservice.configuration.Configuration;
+
+@RestController
+public class LimitsController {
+	
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public Limits retiveLimits() {
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
+	}
+}
