411cddddca176c7c382a57c352341d4e1c56ae90
satishlabs <spd61185@gmail.com>
2023-04-25 18:50:27 +0530

    Currency-Exchange microserice

---
+++ Week-3Labs/currency-exchange-service/src/test/java/com/satishlabs/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satishlabs.microservices.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
