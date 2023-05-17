1a4b48fbcb6b96e7d1092cd2f21dcdd475ed9b84
satishlabs <spd61185@gmail.com>
2023-05-17 11:55:35 +0530

    Updated code

---
+++ Week-3Labs/api-gateway/src/test/java/com/satishlabs/microservices/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satishlabs.microservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
